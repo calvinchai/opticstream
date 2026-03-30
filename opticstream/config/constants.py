be645720e80129ec3065d2d60187383e2cfedfa6
Kaidong Chai <dev@kchai.me>
2026-03-30 16:44:23 -0400

    feat: add on-call person mention for failed message

--- opticstream/config/constants.py
+++ opticstream/config/constants.py
@@ -3,6 +3,7 @@ from typing import Literal
 SLACK_WEBHOOK_BLOCK_NAME = "watchdog"
 SLACK_API_TOKEN_BLOCK_NAME = "watchdog"
 SLACK_CHANNEL_BLOCK_NAME = "watchdog-channel"
+SLACK_ONCALL_VARIABLE_NAME = "oncall-person"
 
 DANDI_API_TOKEN_BLOCK_NAME = "dandi-api-key"
 LINC_API_TOKEN_BLOCK_NAME = "linc-api-key"
--- opticstream/utils/slack_notification_hook.py
+++ opticstream/utils/slack_notification_hook.py
@@ -1,21 +1,24 @@
 from typing import Any
+
 from prefect.blocks.notifications import SlackWebhook
 from prefect.settings import PREFECT_UI_URL
+from prefect.variables import Variable
 
-from opticstream.config.constants import SLACK_WEBHOOK_BLOCK_NAME
+from opticstream.config.constants import SLACK_ONCALL_VARIABLE_NAME, SLACK_WEBHOOK_BLOCK_NAME
 
 
 def slack_notification_hook(flow: Any, flow_run: Any, state: Any) -> None:
     """
     Send a Slack notification when a flow enters a state.
     """
+    oncall_person = Variable.get(SLACK_ONCALL_VARIABLE_NAME, default={})
+    oncall_person_mention = "<@".join([f"<@{person}>" for person in oncall_person.values()])
     slack_webhook_block = SlackWebhook.load(SLACK_WEBHOOK_BLOCK_NAME)
     slack_webhook_block.notify(
         (
             f"Your job {flow_run.name} entered {state.name} "
             f"See <{PREFECT_UI_URL.value()}/runs/"
             f"flow-run/{flow_run.id}|the flow run in the UI>\n\n"
-            f"Tags: {flow_run.tags}\n\n"
-            f"Scheduled start: {flow_run.expected_start_time}"
+            f"Oncall: {oncall_person_mention}"
         )
     )
